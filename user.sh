70fca5777907b6105027f0e7ee531460acb8d08d
sivaganeshthotasl <sivaganeshthotasl@gmail.com>
2026-05-19 14:58:02 +0530

    Creating user Service

--- user.sh
+++ user.sh
@@ -60,7 +60,7 @@ VALIDATE $? "Installing Nodejs"
 
 #####Application Configuration#####
 # Creating Application User
-id roboshop 
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
      useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE
